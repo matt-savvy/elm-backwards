f5731c1b8b37f37e135d2eb9b3526e10701e85d1
Matt Savoia <matt@1-800-rad-dude.com>
2024-02-10 15:34:29 -0500

    Update backwards, show output for failing version

--- backwards.sh
+++ backwards.sh
@@ -12,11 +12,12 @@ reset() {
 check_versions() {
     for i in "${VERSIONS[@]}"; do
         echo "checking tag: $i";
-        git checkout "$i" tests > /dev/null 2>&1 && elm-test >/dev/null 2>&1
+        git checkout "$i" tests > /dev/null 2>&1 && OUTPUT=$(elm-test)
         TEST_RESULT=$?
 
         if [[ $TEST_RESULT -ne 0 ]]; then
             reset
+            echo "$OUTPUT"
             echo "failing tag found: $i"
             return 1;
         fi
