a74ac8a898839ac5da3214cb70f6d3b4092935a3
Matt Savoia <matt@1-800-rad-dude.com>
2024-02-10 15:03:08 -0500

    Update backwards.sh, add check_versions

--- backwards.sh
+++ backwards.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-VERSIONS=$@
+VERSIONS=( "$@" )
 
-for i in $VERSIONS; do
-  echo $i;
-done
+# reset test dir to HEAD
+reset() {
+    git checkout HEAD tests
+
+}
+
+# run current code against the tests for all previous versions
+check_versions() {
+    for i in "${VERSIONS[@]}"; do
+        git checkout "$i" tests && elm-test
+        TEST_RESULT=$?
+        echo "TEST RESULT $TEST_RESULT"
+
+        if [[ $TEST_RESULT -ne 0 ]]; then
+            reset
+            echo "failing tag $i"
+            return 1;
+        fi
+    done
+
+    echo "Passes tests for previous tags"
+    reset
+    return 0
+}
+
+check_versions
+exit $?
